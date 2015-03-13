e742a7d5931285073d4dc63819e97e3e208cc1c3
Pedro Paulo Amorim <pp.amorim@hotmail.com>
2015-03-13 09:27:09 -0300

    cahnges on styles and project structure

--- app/src/main/java/com/ppamorim/dragger/BaseActivity.java
+++ app/src/main/java/com/ppamorim/dragger/BaseActivity.java
@@ -2,6 +2,7 @@ package com.ppamorim.dragger;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import com.ppamorim.dragger.app.R;
 
 public class BaseActivity extends ActionBarActivity {
 
